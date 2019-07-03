21bfcbc4d601abc3bdb56d62c980d0e6b96bd91b
Phil Davidson <phildavidson101@gmail.com>
2019-07-03 14:19:47 +0100

    - Added contact details to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(name='pyCIPAPI',
       version='0.1.0',
+      author="NHS Bioinformatics Group",
+      author_email="joowook.ahn@nhs.net",
       description='Python library for access the GELs CPI API',
+      license="TBC",
       url='https://github.com/NHS-NGS/JellyPy',
       packages=['pyCIPAPI'],
       install_requires=[
